44ff264b54584155adc844234e23221016a6ef52
unknown <adri130497@gmail.com>
2019-09-26 14:58:53 -0500

    Login y Registro

---
+++ forms.py
@@ -0,0 +1,14 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField, BooleanField
+# from wtforms.validators import DataRequired, Length, Email, EqualTo
+#
+#
+# class RegistrationForm(FlaskForm):
+#     email = StringField('Email',validators=[DataRequired(), Email()])
+#     password=PasswordField('Password', validators=[DataRequired()])
+#     submit = SubmitField('Registro')
+#
+# class LoginForm(FlaskForm):
+#     email = StringField('Email',validators=[DataRequired(), Email()])
+#     password=PasswordField('Password', validators=[DataRequired()])
+#     submit = SubmitField('Login')
--- mysql.py
+++ mysql.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,render_template
+from flask import Flask, request,render_template, url_for, flash, redirect
 from flaskext.mysql import MySQL
 from flask_mysqldb import MySQL
 
@@ -9,23 +9,45 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'admin'
 app.config['MYSQL_DB'] = 'wbc_final'
 app.config['MYSQL_HOST'] = 'localhost'
+app.config['SECRET_KEY'] = '6023611b18e90c93803a8dd783540183'
+
 mysql = MySQL(app)
 
-@app.route('/')
+@app.route('/login')
 def myform():
-    return render_template('from_ex.html')
+    return render_template('login.html')
 
-@app.route('/',methods=['POST'])
-def Authenticate():
-    username=request.form['u']
-    password=request.form['p']
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login',methods=['POST'])
+def login():
+    username=request.form['user']
+    password=request.form['pass']
     cursor=mysql.connect.cursor()
     cursor.execute("SELECT * from usuarios where email='" + username + "'and contrasena='" + password + "'")
     data=cursor.fetchone()
     if data is None:
-        return "Wrong username or password"
+        flash("Usuario o Contraseña Incorrectos")
     else:
-        return render_template('home.html')
+        return redirect(url_for('home'))
+
+
+@app.route('/register',methods=['GET', 'POST'])
+def register():
+     return render_template('register.html',title='Registro')
+#
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         if form.email.data == 'u' and form.password.data== 'p':
+#             return redirect(url_for('home'))
+#     return render_template('login.html',title='Login', form=form)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
