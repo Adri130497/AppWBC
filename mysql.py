2fa9740aee2bb8b1725d3cecac2c2207d0e55e68
Sergio Chavez (serchave) <serchave@cisco.com>
2019-09-27 00:13:32 -0500

    Cambio en pantalla de boxeador

--- mysql.py
+++ mysql.py
@@ -34,6 +34,39 @@ def login():
     else:
         return redirect(url_for('home'))
 
+@app.route('/login',methods=['POST'])
+def add_boxer():
+    nombre=request.form['nombre-boxeador']
+    apellido_pat=request.form['apellido-pat-boxeador']
+    apellido_mat=request.form['apellido-mat-boxeador']
+    email=request.form['email']
+    password=request.form['password']
+    birthDate=request.form['birthDate']
+    tel_casa=request.form['tel-casa']
+    tel_celular=request.form['tel-celular']
+    fecha_regis=request.form['fecha-regis']
+    calle=request.form['calle-boxeador']
+    calle_num=request.form['calle-num-boxeador']
+    colonia=request.form['colonia-boxeador']
+    municipio=request.form['municipio-boxeador']
+    cp=request.form['cp-boxeador']
+    estado=request.form['estado-boxeador']
+    foto=request.form['foto-boxeador']
+    guardia=request.form['guardia-boxeador']
+    sangre=request.form['sangre-boxeador']
+    estatura=request.form['estatura-boxeador']
+    peso=request.form['peso-boxeador']
+    alias=request.form['alias-boxeador']
+    peleas=request.form['peleas-boxeador']
+    cursor=mysql.connect.cursor()
+    cursor.execute("INSERT INTO users (nombre,apellido_paterno,apellido_materno,fecha_nacimiento,telefono_casa,celular,email,contrasena,fecha_registro,calle,numero,colonia,municipio,cp,estado,foto) values ("nombre,apellido_pat,apellido_mat,birthDate,tel_casa,tel_celularemail,password,fecha_regis,calle,calle_num,colonia,municipio,cp,estado,foto")")
+    cursor.execute("INSERT INTO boxeadores (guardia,tipo_sangre,estatura,peso,alias,num_peleas) values ("guardia,sangre,estatura,peso,alias,peleas")")
+    data=cursor.fetchone()
+    if data is None:
+        flash("Llene los campos obligatorios")
+    else:
+        return redirect(url_for('home'))
+
 
 @app.route('/register',methods=['GET', 'POST'])
 def register():
